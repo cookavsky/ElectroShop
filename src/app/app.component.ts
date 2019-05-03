896e25a572af808a0976a60817dd8ea0514d06a1
cookavsky <cookavsky@gmail.com>
2019-05-03 23:59:35 +0200

    -footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,63 +10,23 @@ export class AppComponent {
 
   constructor(private ren: Renderer2) {
   }
-  enteredButton = false;
-  isMatMenuOpen = false;
-  isMatMenu2Open = false;
-  prevButtonTrigger;
+
+  focusBtn = false;
+  prevEmptyTrigger;
 
   buttonFocus(trigger) {
-    setTimeout(() => {
-      if (this.prevButtonTrigger && this.prevButtonTrigger != trigger) {
-        this.prevButtonTrigger.closeMenu();
-        this.prevButtonTrigger = trigger;
-        this.isMatMenuOpen = false;
-        this.isMatMenu2Open = false;
-        trigger.openMenu()
-      }
-      else if (!this.isMatMenuOpen) {
-        this.enteredButton = true;
-        this.prevButtonTrigger = trigger
-        trigger.openMenu()
-      }
-      else {
-        this.enteredButton = true;
-        this.prevButtonTrigger = trigger
-      }
-    })
-  }
-
-  buttonLeave(trigger, button) {
-    setTimeout(() => {
-      if (this.enteredButton && !this.isMatMenuOpen) {
-        trigger.closeMenu();
-        this.ren.removeClass(button['_elementRef'].nativeElement, 'cdk-focused');
-        this.ren.removeClass(button['_elementRef'].nativeElement, 'cdk-program-focused');
-      } if (!this.isMatMenuOpen) {
-        trigger.closeMenu();
-        this.ren.removeClass(button['_elementRef'].nativeElement, 'cdk-focused');
-        this.ren.removeClass(button['_elementRef'].nativeElement, 'cdk-program-focused');
-      } else {
-        this.enteredButton = false;
-      }
-    }, 100)
-  }
-  menuLeave(trigger, button) {
-    setTimeout(() => {
-      if (!this.isMatMenu2Open && !this.enteredButton) {
-        this.isMatMenuOpen = false;
-        trigger.closeMenu();
-        this.ren.removeClass(button['_elementRef'].nativeElement, 'cdk-focused');
-        this.ren.removeClass(button['_elementRef'].nativeElement, 'cdk-program-focused');
-      } else {
-        this.isMatMenuOpen = false;
-      }
-    }, 80)
-  }
-  menuenter() {
-    this.isMatMenuOpen = true;
-    if (this.isMatMenu2Open) {
-      this.isMatMenu2Open = false;
+    if (!this.focusBtn) {
+      this.focusBtn = true;
+      this.prevEmptyTrigger = trigger
+      trigger.openMenu();
+    } else if (this.prevEmptyTrigger && this.prevEmptyTrigger != trigger) {
+      this.prevEmptyTrigger.closeMenu();
+      this.prevEmptyTrigger = trigger;
+      trigger.openMenu();
+    } else {
+      this.focusBtn = true;
+      this.prevEmptyTrigger = trigger;
+      trigger.openMenu();
     }
   }
 }
